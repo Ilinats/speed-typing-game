ef08154488a8bef556ac4a8a3acdaaf85dbaa7e4
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-05-06 17:36:26 +0300

    doesn't work.

--- objects.py
+++ objects.py
@@ -1,19 +1,12 @@
-import pygame, random
+import pygame
+import random
 
-screen = pygame.display.set_mode((720, 1280))
-SCREEN = WIDTH, HEIGHT = 720, 1280
-WORD_WIDTH = WIDTH // 4
-BLACK = (0, 0, 0)
-
-import pygame, random
-
-screen = pygame.display.set_mode((720, 1280))
 SCREEN = WIDTH, HEIGHT = 720, 1280
 WORD_WIDTH = WIDTH // 4
 BLACK = (0, 0, 0)
 
 class Word(pygame.sprite.Sprite):
-    def __init__(self, text, speed, delay, spacing, positions):
+    def __init__(self, text, speed, delay, spacing, positions, word_group):
         super().__init__()
         self._delay = delay
         self._letters = list(text)
@@ -23,10 +16,13 @@ class Word(pygame.sprite.Sprite):
         self._for_position = text
         self._text = self._letters[::-1]
         self._x = random.randint(25, WIDTH-25)
-        self._y = -45 * len(text)
-        self._spacing = spacing  # spacing between words
+        self._y = -45 * len(text) - spacing
+        self._spacing = spacing
         self.rect = pygame.Rect(self._x, self._y, WORD_WIDTH, len(text) * WORD_WIDTH)
         self._positions = positions
+        self.current_letter = self._letters[0]
+        self._word_group = word_group  # add word group attribute
+        self._word_group.add(self)  # add self to word group
 
     def update(self):
         if self._delay > 0:
@@ -35,21 +31,42 @@ class Word(pygame.sprite.Sprite):
             self._y += self._speed
             if self._y > HEIGHT:
                 self.kill()
+                return
 
-                # Checks for overlapping with other words
-                for i, pos in enumerate(self._positions):
-                    if self.rect.colliderect(pygame.Rect(self._x, pos, WORD_WIDTH, len(self._for_position) * WORD_WIDTH)):
-                        self._y = pos - len(self._for_position) * WORD_WIDTH - self._spacing
+            for i, pos in enumerate(self._positions):
+                if self.rect.colliderect(pygame.Rect(self._x, pos, WORD_WIDTH, len(self._for_position) * WORD_WIDTH)):
+                    self._y = pos - len(self._for_position) * WORD_WIDTH - self._spacing
+                    self.rect = pygame.Rect(self._x, self._y, WORD_WIDTH, len(self._for_position) * WORD_WIDTH)
+                    break
 
-                self.rect = pygame.Rect(self._x, self._y, WORD_WIDTH, len(self._for_position) * WORD_WIDTH)
+            if self._delay == 0 and self._letters:
+                self.current_letter = self._letters[0]
+                self._letters.pop(0)
+                self._text = self._letters[::-1]
 
+            # Update position of the word
+            self.rect.y = self._y
+       
     def draw(self, surface):
+        self.image = pygame.Surface((WORD_WIDTH, len(self._text) * WORD_WIDTH), pygame.SRCALPHA)
+        self.image.fill((255, 255, 255, 0))
         chars = list(self._text)
         char_height = self._font.size(" ")[1]
 
         for i, char in enumerate(chars):
             char_surface = self._font.render(char, True, self._color)
             char_rect = char_surface.get_rect()
-            char_rect.centerx = self._x
-            char_rect.bottom = self._y + (i+1)*char_height
-            surface.blit(char_surface, char_rect)
+            char_rect.centerx = self.rect.width / 2
+            char_rect.bottom = (i + 1) * char_height
+            self.image.blit(char_surface, char_rect)
+
+        surface.blit(self.image, self.rect)
+
+            
+    def check_letter(self, letter):
+        if self._text and letter == self._text[-1]:
+            self._text.pop()
+            if not self._text:
+                self.kill()
+            return True
+        return False
