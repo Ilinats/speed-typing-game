58c90d2cac043277f050da8d1e35d55d02e07043
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-05-08 16:26:47 +0300

    not going off screen?

--- objects.py
+++ objects.py
@@ -10,14 +10,13 @@ class Word(pygame.sprite.Sprite):
     def __init__(self, level):
         super().__init__()
         self._level = level
-        self._y = -45
-        self._x = random.randint(25, WIDTH - 25)
         self._delay = 300 // self._level
         self._speed = 1 * self._level
         word = ''.join((random.choice(curr_chars)) for x in range(len(curr_chars)))
         self._font = pygame.font.SysFont('comicsans', 30)
         self._text = self._font.render(word, True, BLACK)
-        #self._should_delay = 1
+        self._y = -45
+        self._x = random.randint(25, WIDTH - 10 * len(word))
 
     def update(self):
         if self._delay:
