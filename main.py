b8b12e073582373995b25efe6db8057e029a998b
Ilinats <107075825+Ilinats@users.noreply.github.com>
2023-05-08 16:03:11 +0300

    restart - horizontal word movement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, random
+import pygame
 from objects import Word
 
 pygame.init()
@@ -9,21 +9,7 @@ background_color = (255, 255, 255)
 clock = pygame.time.Clock()
 FPS = 60
 
-# Creating a word group
-word_group = pygame.sprite.Group()
-
-# List of test words
-word_list = ["Test"]
-
-# Adding the words from the list
-word_positions = []
-spacing = 100
-
-for i, word in enumerate(word_list):
-    delay = i * 60  # Delay each word by 1 second
-    word_obj = Word(word, 5, delay, spacing, word_positions)
-    word_group.add(word_obj)
-    word_positions.append(word_obj.rect.y)
+word = Word(1)
 
 running = True
 
@@ -38,10 +24,8 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-
-    word_group.update()
-    for word in word_group:
-        word.draw(screen)
+    
+    word.update()
 
     clock.tick(FPS)
     pygame.display.update()
--- objects.py
+++ objects.py
@@ -1,55 +1,26 @@
-import pygame, random
+import pygame, random, string
 
-screen = pygame.display.set_mode((720, 1280))
 SCREEN = WIDTH, HEIGHT = 720, 1280
-WORD_WIDTH = WIDTH // 4
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 BLACK = (0, 0, 0)
 
-import pygame, random
-
-screen = pygame.display.set_mode((720, 1280))
-SCREEN = WIDTH, HEIGHT = 720, 1280
-WORD_WIDTH = WIDTH // 4
-BLACK = (0, 0, 0)
+curr_chars = 'asjkl'
 
 class Word(pygame.sprite.Sprite):
-    def __init__(self, text, speed, delay, spacing, positions):
+    def __init__(self, level):
         super().__init__()
-        self._delay = delay
-        self._letters = list(text)
-        self._font = pygame.font.SysFont("Arial", 36)
-        self._color = (0, 0, 0)
-        self._speed = speed
-        self._for_position = text
-        self._text = self._letters[::-1]
-        self._x = random.randint(25, WIDTH-25)
-        self._y = -45 * len(text)
-        self._spacing = spacing  # spacing between words
-        self.rect = pygame.Rect(self._x, self._y, WORD_WIDTH, len(text) * WORD_WIDTH)
-        self._positions = positions
+        self._level = level
+        self._y = -25
+        self._x = random.randint(25, WIDTH - 25)
+        self._delay = 300 // self._level
+        self._speed = 1 * self._level
+        word = ''.join((random.choice(curr_chars)) for x in range(len(curr_chars)))
+        self._font = pygame.font.SysFont('comicsans', 30)
+        self._text = self._font.render(word, True, BLACK)
 
     def update(self):
-        if self._delay > 0:
-            self._delay -= 1
-        else:
-            self._y += self._speed
-            if self._y > HEIGHT:
-                self.kill()
-
-                # Checks for overlapping with other words
-                for i, pos in enumerate(self._positions):
-                    if self.rect.colliderect(pygame.Rect(self._x, pos, WORD_WIDTH, len(self._for_position) * WORD_WIDTH)):
-                        self._y = pos - len(self._for_position) * WORD_WIDTH - self._spacing
-
-                self.rect = pygame.Rect(self._x, self._y, WORD_WIDTH, len(self._for_position) * WORD_WIDTH)
-
-    def draw(self, surface):
-        chars = list(self._text)
-        char_height = self._font.size(" ")[1]
-
-        for i, char in enumerate(chars):
-            char_surface = self._font.render(char, True, self._color)
-            char_rect = char_surface.get_rect()
-            char_rect.centerx = self._x
-            char_rect.bottom = self._y + (i+1)*char_height
-            surface.blit(char_surface, char_rect)
+        self._y += self._speed
+        if self._y > HEIGHT:
+            self.kill()
+            
+        screen.blit(self._text, (self._x, self._y))
